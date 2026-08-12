df253a662ab122a291f29a584a9015061b48e88c
Vedant Shah <vedantshah1110@gmail.com>
2026-08-12 17:08:25 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans=0,cnt=0;
+        for(int num : nums)
+        {
+            if(cnt==0)
+            {
+                ans = num;
+            }
+            if(num == ans)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
